179edb0e607e532af488974dfc20acfec7f1b530
iwilldev <238383632+iwilldev@users.noreply.github.com>
2026-03-09 10:58:29 -0400

    Create WeightedGraph.py and lay out the class, methods and comments within.

---
+++ WeightedGraph.py
@@ -0,0 +1,30 @@
+# WeightedGraph.py
+# Written by Alex Barbee
+
+class WeightedGraph:
+    def __init__(self, fname):
+        self.fileName = fname
+
+
+    # Calculates the weight of the edge between nodes
+    # @i and @k, and results in -1 if they do not exist
+    # @return: a float value being the weight
+    def getWeight(i, k):
+        pass
+
+    # Checks whether an edge exists between @i and @k
+    # or if @i and @k are the same node
+    # @return: A boolean value if edge exists
+    def directLink(i,k):
+        pass
+    
+    # Gets the name of a node associated with 
+    # the given index @i
+    # @return: A string being the name of the node
+    def getNodeName(i):
+        pass
+
+    # Gets the current number of nodes in the graph
+    # @return an integer count of nodes
+    def getSize():
+        pass
