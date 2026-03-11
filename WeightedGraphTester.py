c9d9bc45ad0bde4933eac38918db95f0cfa25f63
iwilldev <238383632+iwilldev@users.noreply.github.com>
2026-03-10 23:22:40 -0400

    Populate tester script with comments

--- WeightedGraphTester.py
+++ WeightedGraphTester.py
@@ -1,14 +1,18 @@
 import WeightedGraph as wg
 import pandas as pd
 
+
+# Initialize Matrix Variables
 testMatrix = wg.WeightedGraph("example.txt")
 testMatrix2 = wg.WeightedGraph("example2.txt")
 
+
+#tests the getWeight function
 def weightTest(i,k,exp,mtx):
     ret = mtx.getWeight(i,k)
     print(f"getWeight({i},{k}) returns: {ret}   expected: {exp}\n")
 
-
+#creates LaTeX output using the Panda Dataframe
 def makeLatex(mtx):
     table = pd.DataFrame(mtx.matrix, columns=list("".join(mtx.names)))
     table.insert(loc=0, column="", value=mtx.names)
@@ -16,6 +20,7 @@ def makeLatex(mtx):
     latex_out = table.to_latex(index=False)
     return latex_out
 
+#Prints in depth tests of matrices
 def printSpecifics(theMatrix,showtex):
     print("***** Printing matricies *****")
     print(theMatrix.matrix)
@@ -29,15 +34,17 @@ def printSpecifics(theMatrix,showtex):
     print(theMatrix.getSize())
 
 
+
+#Test Execution
 print("\n\nEXAMPLE MATRIX TESTING:\n\n")
 print("***** Testing getWeight *****")
 weightTest(0,0,0,testMatrix)
 weightTest(0,1,4,testMatrix)
 weightTest(5,3,6,testMatrix)
 
-printSpecifics(testMatrix,False)
+printSpecifics(testMatrix,True)
 print("\n\nUSER-MADE MATRIX 1 TESTING:\n\n")
 weightTest(0,0,0,testMatrix2)
 weightTest(0,1,1,testMatrix2)
 weightTest(5,3,6,testMatrix2)
-printSpecifics(testMatrix2,False)
+printSpecifics(testMatrix2,True)
