2ef8d8730fc9fbe21d8739f8c594359618fd6684
iwilldev <238383632+iwilldev@users.noreply.github.com>
2026-03-27 20:53:53 -0400

    Fixed complexity issue. Still not correct output.

--- PathAlgorithm.py
+++ PathAlgorithm.py
@@ -13,10 +13,10 @@ def _getSmallest(array):
         return 0
     return currentLeast
 
-def _getSmallestNot(array, notint):
+def _getSmallestNot(array, notin):
     currentLeast = float('inf')
     for iterable in array:
-        if iterable > 0 and iterable < currentLeast and iterable != notint:
+        if iterable > 0 and iterable < currentLeast and iterable not in notin:
             currentLeast = iterable
     if currentLeast == float('inf'):
         return 0
@@ -39,50 +39,60 @@ def prim(WeightedGraphObj):
 
     # here, we add the current node as the first item in the node list
     # because we have to start from somewhere
-    nodes = [curNode]
+    #nodes = [curNode]
 
     #we set the first point in the table to 0, since we pick it first.
     distanceTable[curNode] = 0
 
-    #loop through each node, and perform the algorithm
+    # loop through each node in the adjacency matrix.
+    # Only the number of times matters as the order may differ
     for i in range(length):
-        for k in range(length):
-            # here we get the weight from the current edge (of many) branching from the current node
-            item = int(graph[curNode][k])
 
-        
-            if item > 0:
-                if item < distanceTable[k] or distanceTable[k] == -1:
-                    distanceTable[k] = item
+        #first we add our current node to the visited list
+        nodes.append(curNode)
 
+        #Loop through each visited node
+        for eachNode in nodes:
+            #we now loop through all node connections for curNode in the adjacency matrix
+            for connection in range(length):
+
+                # Set item to the currently inspected weight
+                item = int(graph[eachNode][connection])
+
+                #insert item at its index to the distance table if it is not zeroed out
+                #our distance table updates as we gain more nodes
+                if item > 0 and connection not in nodes:
+                    if item < distanceTable[connection] or distanceTable[connection] == -1:
+                        distanceTable[connection] = item
+
+        # Get the smallest available edge weight / distance
         currentLeast = _getSmallest(distanceTable)
         
         # we get the index of the smallest distance found for the current node
         # and store it as nextNode
-        print(currentLeast)
+        
         nextNode = distanceTable.index(currentLeast)
 
-        print("after:")
-        print(distanceTable)
+        #add to the mst
+        mst.add((curNode,nextNode)) 
 
+
+        #print info
+        print("****Iteration****")
+        print("Table:", distanceTable)
+
+        print("Pulling: ",currentLeast)
+        print("Node: (n) ", curNode)
+        
+        print("NodeList: ", nodes)
+        print("MST: ", mst)
         # Since we just pulled the smallest distance, we can remove it from the distance table
         distanceTable[nextNode] = 0
 
-
-        #append the nextNode to the node list
-        if len(nodes) <= length-1:
-            nodes.append(nextNode)
-
-        #here, we get the node with the smallest branch
-        smallestBranch = float('inf')
-        for each in nodes:
-            possible = _getSmallestNot(graph[each],each)
-            if possible < smallestBranch:
-                smallestBranch = possible
-        mst.add((curNode,nextNode))
-
-
-
+        #print(smallestBranch)
+        print("curNode: ", curNode)
+        print("nextNode: ", nextNode)
+        
         # progress forward by iterating the curNode variable to the next node
         curNode = nextNode
     print(nodes)
