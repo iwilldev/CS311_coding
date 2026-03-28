1c4140a2d29e851232bc019103b8386ab2167934
iwilldev <238383632+iwilldev@users.noreply.github.com>
2026-03-28 00:47:54 -0400

    Fixed Prim's algorithm so that it returns correct results. Got totalWeight and edgeName functions working.

--- PathAlgorithm.py
+++ PathAlgorithm.py
@@ -2,25 +2,6 @@
 # Written by Alex Barbee
 
 from WeightedGraph import WeightedGraph
-import numpy as np
-
-def _getSmallest(array):
-    currentLeast = float('inf')
-    for iterable in array:
-        if iterable > 0 and iterable < currentLeast:
-            currentLeast = iterable
-    if currentLeast == float('inf'):
-        return 0
-    return currentLeast
-
-def _getSmallestNot(array, notin):
-    currentLeast = float('inf')
-    for iterable in array:
-        if iterable > 0 and iterable < currentLeast and iterable not in notin:
-            currentLeast = iterable
-    if currentLeast == float('inf'):
-        return 0
-    return currentLeast
 
 def prim(WeightedGraphObj):
     mst = set()
@@ -34,74 +15,83 @@ def prim(WeightedGraphObj):
 
     # initialize the currentnode to the first node in the set
     curNode = 0
-    # for now we do not know the next node so we initialize it as the currentnode
-    nextNode = curNode  
-
-    # here, we add the current node as the first item in the node list
-    # because we have to start from somewhere
-    #nodes = [curNode]
 
     #we set the first point in the table to 0, since we pick it first.
     distanceTable[curNode] = 0
 
-    # loop through each node in the adjacency matrix.
-    # Only the number of times matters as the order may differ
-    for i in range(length):
+    # We loop while there are still nodes we can visit. 
+    # When there are no more, the curNode stays at -1 until the end of the loop iteration
+    while curNode != -1:
 
         #first we add our current node to the visited list
         nodes.append(curNode)
 
-        #Loop through each visited node
+        #prime the variables for the new iteration of the loop
+        nextNode = -1
+        curNode = -1
+        currentLeast = float('inf')
+
+        # Loop through **each node** within the **nodes** list and find the smallest connecting edge 
+        # that has not already been traversed
         for eachNode in nodes:
             #we now loop through all node connections for curNode in the adjacency matrix
             for connection in range(length):
 
                 # Set item to the currently inspected weight
-                item = int(graph[eachNode][connection])
-
-                #insert item at its index to the distance table if it is not zeroed out
-                #our distance table updates as we gain more nodes
+                item = int(WeightedGraphObj.getWeight(eachNode,connection))
+                
                 if item > 0 and connection not in nodes:
-                    if item < distanceTable[connection] or distanceTable[connection] == -1:
+                    #If we make it here, we have found a new smallest edge in an untraversed node
+                    if item < currentLeast:
+                        currentLeast = item
+                        nextNode = connection
+                        curNode = eachNode
+
+                    #insert item at its index to the distance table if it is not zeroed out
+                    #our distance table updates as we gain more nodes
+                    if distanceTable[connection] == -1 or item < distanceTable[connection]:
                         distanceTable[connection] = item
-
-        # Get the smallest available edge weight / distance
-        currentLeast = _getSmallest(distanceTable)
-        
-        # we get the index of the smallest distance found for the current node
-        # and store it as nextNode
-        
-        nextNode = distanceTable.index(currentLeast)
-
-        #add to the mst
-        mst.add((curNode,nextNode)) 
+                    
 
 
+        #if we get negative 1, there are no more nodes to visit
+        if nextNode != -1:
+            #add the new smallest edge to the mst
+            mst.add((curNode,nextNode)) 
+
+        '''
         #print info
         print("****Iteration****")
         print("Table:", distanceTable)
-
         print("Pulling: ",currentLeast)
         print("Node: (n) ", curNode)
-        
         print("NodeList: ", nodes)
         print("MST: ", mst)
+        print("curNode: ", curNode)
+        print("nextNode: ", nextNode)
+        '''
         # Since we just pulled the smallest distance, we can remove it from the distance table
         distanceTable[nextNode] = 0
 
-        #print(smallestBranch)
-        print("curNode: ", curNode)
-        print("nextNode: ", nextNode)
-        
         # progress forward by iterating the curNode variable to the next node
         curNode = nextNode
-    print(nodes)
+    #print(nodes)
     return mst
 
         
 
 def totalWeight(mst, graph):
-    pass
+    total = 0
+    for tuple in mst:
+        total += graph.getWeight(tuple[0],tuple[1])
+    return total
+
+
 
 def edgeNames(mst,graph):
-    pass
+    edges = []
+    for tuple in mst:
+        edge = [str(graph.names[tuple[0]]),str(graph.names[tuple[1]])]
+        edges.append(edge)
+     
+    return edges
